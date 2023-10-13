31c5bc71361920ce02d0016d565f2b5a726dfad4
regstuff <6016831+regstuff@users.noreply.github.com>
2023-10-13 16:58:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pyttsx3
-engine = pyttsx3.init()
+import rlvoice
+engine = rlvoice.init("coqui_ai_tts")
 txt = """New blood test more convenient for measuring important omega-3 levels"""
-engine.save_to_file(txt, 'test.mp3')
 print('MP3 created')
+engine.save_to_file(txt, 'test.mp3')
 engine.runAndWait()
