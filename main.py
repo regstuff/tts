9384f2e616219e451036d12c6846e1c1f34ee471
regstuff <6016831+regstuff@users.noreply.github.com>
2023-10-13 16:57:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import pyttsx3
 engine = pyttsx3.init()
 txt = """New blood test more convenient for measuring important omega-3 levels"""
 engine.save_to_file(txt, 'test.mp3')
+print('MP3 created')
 engine.runAndWait()
